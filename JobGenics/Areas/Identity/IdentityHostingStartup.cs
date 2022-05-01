9611caf831769638dde9ac786f87de26cb16fd49
ChaosPG <gemisoftx@gmail.com>
2022-05-01 19:57:04 +0700

    <JobGenics Commit 01>

---
+++ JobGenics/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JobGenics.Areas.Identity.Data;
+using JobGenics.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JobGenics.Areas.Identity.IdentityHostingStartup))]
+namespace JobGenics.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<JobGenicsContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("JobGenicsContextConnection")));
+
+                services.AddDefaultIdentity<JobGenicsUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<JobGenicsContext>();
+            });
+        }
+    }
+}
